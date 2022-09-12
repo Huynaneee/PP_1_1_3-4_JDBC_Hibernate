3f62cb9798c0fbfbb672f22b5879a12aeac082ad
Huynaneee <96498829+Huynaneee@users.noreply.github.com>
2022-09-12 21:48:25 +0300

    corrected

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,6 +1,7 @@
 package jm.task.core.jdbc;
 
 import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.service.UserService;
 import jm.task.core.jdbc.service.UserServiceImpl;
 
 import java.util.List;
@@ -8,7 +9,7 @@ import java.util.List;
 public class Main {
     public static void main(String[] args) {
         // реализуйте алгоритм здесь
-        UserServiceImpl service = new UserServiceImpl();
+        UserService service = new UserServiceImpl();
 
         service.createUsersTable();
         service.cleanUsersTable();
